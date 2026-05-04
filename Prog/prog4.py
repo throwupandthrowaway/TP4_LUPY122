6eba66873f8a5f0d4823e3c23572b3adc05820ee
throwupandthrowaway <itsjohnmail04@gmail.com>
2026-05-04 23:45:22 +0200

    Update prog4.py

--- Prog/prog4.py
+++ Prog/prog4.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 n=100
 L=5
 dx=L/(n-1)
-p=np.linspace(0,2,100)
+x=np.linspace(0,2,100)
 
 def normalize(m,delta_x):
     m/=np.linalg.norm(m,axis=0)
@@ -14,13 +14,13 @@ def normalize(m,delta_x):
     return m
 
 normalize(v,dx)
-plt.plot(p,v[0]**2,label=r"$\psi_0$")
-plt.plot(p,v[1]**2,label=r"$\psi_1$")
-plt.plot(p,v[2]**2,label=r"$\psi_2$")
+plt.plot(x,v[0]**2,label=r"$\psi_0$")
+plt.plot(x,v[1]**2,label=r"$\psi_1$")
+plt.plot(x,v[2]**2,label=r"$\psi_2$")
 plt.grid()
 plt.legend()
 plt.title("3 premiers états propres de la fonction d'onde")
 plt.xlabel("p")
-plt.ylabel(r"$E_p$")
+plt.ylabel(r"$|\psi_p|^2$")
 plt.savefig("Graphe_TP4_Q7_n100_L5.png",bbox_inches="tight")
 plt.show()
